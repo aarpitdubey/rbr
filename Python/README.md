ba172a5ac53624bef48f65becf3be69fbc3689d8
Arpit Dubey <aarpitdubey@gmail.com>
2024-01-13 23:25:44 +0530

    Printing Hello World in python

--- Python/README.md
+++ Python/README.md
@@ -12,3 +12,12 @@ print("Hello World)
 print("Hello World Hello World Hello World")
 ```
 ![Alt text](image-1.png)
+
+**Print "Hello World" in python for 3 times in different lines**
+
+```python
+print("Hello World")
+print("Hello World")
+print("Hello World")
+```
+![Alt text](image-2.png)
--- Python/helloworld.py
+++ Python/helloworld.py
@@ -2,5 +2,10 @@
 # print("Hello World")
 
 # Q. Print "Hello World" in python for 3 times
-print("Hello World Hello World Hello World")
+# print("Hello World Hello World Hello World")
+
+# Q. Print "Hello World" in python for 3 times in different lines
+print("Hello World")
+print("Hello World")
+print("Hello World")
 
