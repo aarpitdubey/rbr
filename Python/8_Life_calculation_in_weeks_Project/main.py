be91dcadc113a631f7f8234ec4ca5944729dc915
Arpit Dubey <aarpitdubey@gmail.com>
2024-01-14 22:28:08 +0530

    Life Calculator Project in python completed !

---
+++ Python/8_Life_calculation_in_weeks_Project/main.py
@@ -0,0 +1,13 @@
+print("\n"+"*"*30+" Life Calculator "+"*"*30+"\n")
+
+age               = input("\nEnter your age : ")
+age_as_int        = int(age)
+years_remaining   = 90 - age_as_int
+weeks_remaining   = years_remaining * 52
+months_remaining  = years_remaining * 12
+days_remaining    = years_remaining * 365
+message = f"\nYou have {years_remaining} years, \
+{months_remaining} months, {months_remaining} months, \
+{weeks_remaining} weeks, {days_remaining} days"
+
+print(message)
